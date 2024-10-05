68d8ba889f8b1282fc3870981b241ddd9674cc05
PunGy <max.yakovlev555@gmail.com>
2024-10-06 02:30:11 +0300

    shit coding continue

--- src/game/scenes/Level.ts
+++ src/game/scenes/Level.ts
@@ -1,8 +1,9 @@
+import { Node } from "engine:/nodes/Node";
 import { CanvasNode } from "engine:/nodes/Node/CanvasNode";
 import { Scene } from "engine:/scenes/Scene";
 import { ServiceProvider } from "engine:/services/ServiceProvider";
 
-const randomInt = (max: number, min = 0) => Math.floor(Math.random() * (max - min) + min);
+const randomInt = (min: number, max = 0) => Math.floor(Math.random() * (max - min) + min);
 
 class Background extends CanvasNode<Level> {
     init() {
@@ -16,8 +17,6 @@ class Background extends CanvasNode<Level> {
             Array.from({ length: randomInt(50, 70) })
                 .map(() => ({ x: randomInt(0, this.width), y: randomInt(0, this.height) }))
 
-        console.log(stars)
-
         this.safeSubscribe(this.$renderSignal, () => {
             render
                 .rect(0, 0, this.width, this.height, 'black')
@@ -29,10 +28,62 @@ class Background extends CanvasNode<Level> {
     }
 }
 
+class Platform extends CanvasNode<Level> {
+    color: string = '#d9e532'
+
+    constructor(level: Level) {
+        super(level);
+        this.height = 10;
+        this.width = 100;
+    }
+
+    init() {
+        const render = ServiceProvider.get('RenderService')
+        this.safeSubscribe(this.$renderSignal, () => {
+            render.rect(this.position.x, this.position.y, this.width, this.height, this.color);
+        })
+    }
+}
+
+class PlatformManager extends Node<Level> {
+    platforms: Array<Platform> = [];
+
+    init() {
+        const viewportService = ServiceProvider.get('ViewportService');
+        this.width = viewportService.width;
+        this.height = viewportService.height;
+        const spaceBetween = 150
+
+        let prevY = 0;
+        const segmentsCount = this.height / spaceBetween;
+        this.platforms = Array.from({ length: segmentsCount })
+            .map((_, i) => {
+                const platform = new Platform(this.scene);
+                const y = prevY + spaceBetween;
+
+                platform.position.y = y;
+                platform.position.x = i === (segmentsCount - 2)
+                    ? this.width / 2 - platform.width / 2
+                    : randomInt(
+                        100, this.width - 100,
+                    )
+                prevY = y;
+
+                return platform;
+            });
+
+        this.platforms.forEach(platform => {
+            this.scene.addNode(platform);
+        })
+    }
+}
+
 export class Level extends Scene {
     constructor() {
         super()
         const background = new Background(this);
+        const platformManager = new PlatformManager(this)
         this.addNode(background);
+        this.addNode(platformManager)
     }
 }
