7aeee0c6db8f8f27b1504b46288a07795146123a
Maksim Iakovlev <max.yakovlev555@gmail.com>
2026-08-05 13:05:57 +1000

    Revert "prepare to make it monorepo"
    This reverts commit 924f89b762d8b728172e29accaff4ee8e1f441b4.

--- games/spaceInvaders/src/components/EnemyOperator/EnemyOperator.ts
+++ games/spaceInvaders/src/components/EnemyOperator/EnemyOperator.ts
@@ -164,7 +164,7 @@ export class EnemyOperator extends CanvasNode<MainScene> {
             } else {
                 speedIncreaseOn = 30
             }
-
+    
             if (scene.level === 1) {
                 maxSpeed = 500
             } else if (scene.level === 2) {
